b89f295d0aa95492c244c6328d4faca295a2c4c7
David Ang <davidang09@gmail.com>
2020-01-15 17:17:21 +0800

    initial setup of moving sprite on top of a youtube video

--- src/index.js
+++ src/index.js
@@ -1,32 +1,60 @@
-import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
+import Phaser from "phaser"
+import logoImg from "./assets/logo.png"
+import adventurerJson from "./assets/adventurer.json"
+import adventurerSheet from "./assets/adventurer.png"
 
 const config = {
   type: Phaser.AUTO,
-  parent: "phaser-example",
-  width: 800,
-  height: 600,
+  parent: "phaserCanvas",
+  transparent: true,
+  pixelArt: true,
+  roundPixels: true,
+  width: 896,
+  height: 504,
   scene: {
     preload: preload,
     create: create
   }
-};
+}
 
-const game = new Phaser.Game(config);
+const game = new Phaser.Game(config)
 
 function preload() {
-  this.load.image("logo", logoImg);
+  this.load.atlas('adventurer', adventurerSheet, adventurerJson)
 }
 
 function create() {
-  const logo = this.add.image(400, 150, "logo");
+  const idleFrames = this.anims.generateFrameNames('adventurer', {
+    start: 0, end: 4, zeroPad: 2,
+    prefix: 'adventurer-idle-', suffix: '.png'
+  })
+  const runFrames = this.anims.generateFrameNames('adventurer', {
+    start: 0, end: 5, zeroPad: 2,
+    prefix: 'adventurer-run-', suffix: '.png'
+  })
+  this.anims.create({ key: 'idle', frames: idleFrames, frameRate: 8, repeat: -1 })
+  this.anims.create({ key: 'run', frames: runFrames, frameRate: 10, repeat: -1 })
+
+  const adventurer = this.add.sprite(100, 370, 'adventurer', 'adventurer-slide-00.png')
+  adventurer.setScale(4, 4)
+  adventurer.anims.play('idle')
 
   this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
+    targets: adventurer,
+    x: { from: 100, to: 800 },
+    delay: 5000,
+    onStart: () => {
+      adventurer.anims.play('run')
+    },
+    onRepeat: () => {
+      adventurer.setScale(4, 4)
+    },
+    onYoyo: () => {
+      adventurer.setScale(-4, 4)
+    },
+    ease: 'Linear',
+    duration: 3000,
+    repeat: -1,
+    yoyo: true
+  })
 }

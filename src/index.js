f440e2ac5c638e4c9e4e23a9dd45bcb7565185f7
David Ang <davidang09@gmail.com>
2020-01-16 17:17:22 +0800

    interface with rabbitmq to receive and execute basic commands

--- src/index.js
+++ src/index.js
@@ -18,9 +18,52 @@ const config = {
 }
 
 const game = new Phaser.Game(config)
+let adventurer, running
 
 function preload() {
   this.load.atlas('adventurer', adventurerSheet, adventurerJson)
+
+  let ws = new WebSocket('ws://159.89.201.140:15674/ws');
+  let client = Stomp.over(ws);
+
+  var on_connect = function() {
+    let id = client.subscribe("/topic/test", function(d) {
+        if(['attack', 'jump'].includes(d.body)) {
+          if(running) return
+          adventurer.anims.play(d.body)
+        }
+        if(d.body == 'run') {
+          const scene = game.scene.scenes[0]
+          scene.tweens.add({
+            targets: adventurer,
+            x: { from: 100, to: 800 },
+            onStart: () => {
+              running = true
+              adventurer.anims.play('run')
+            },
+            onComplete: () => {
+              running = false
+              adventurer.setScale(4, 4)
+              adventurer.anims.play('idle')
+            },
+            onRepeat: () => {
+              adventurer.setScale(4, 4)
+            },
+            onYoyo: () => {
+              adventurer.setScale(-4, 4)
+            },
+            ease: 'Linear',
+            duration: 3000,
+            repeat: 0,
+            yoyo: true
+          })
+        }
+    })
+  }
+  var on_error =  function() {
+    console.log('error')
+  }
+  client.connect('guest', 'guest', on_connect, on_error, '/')
 }
 
 function create() {
@@ -32,29 +75,37 @@ function create() {
     start: 0, end: 5, zeroPad: 2,
     prefix: 'adventurer-run-', suffix: '.png'
   })
+  const attackFrames = this.anims.generateFrameNames('adventurer', {
+    start: 0, end: 5, zeroPad: 2,
+    prefix: 'adventurer-attack2-', suffix: '.png'
+  })
+  const jumpFrames = this.anims.generateFrameNames('adventurer', {
+    start: 0, end: 3, zeroPad: 2,
+    prefix: 'adventurer-jump-', suffix: '.png'
+  })
+  const sheatheFrames = this.anims.generateFrameNames('adventurer', {
+    start: 0, end: 3, zeroPad: 2,
+    prefix: 'adventurer-swrd-shte-', suffix: '.png'
+  })
+
   this.anims.create({ key: 'idle', frames: idleFrames, frameRate: 8, repeat: -1 })
   this.anims.create({ key: 'run', frames: runFrames, frameRate: 10, repeat: -1 })
+  this.anims.create({ key: 'jump', frames: jumpFrames, frameRate: 7, repeat: 0, yoyo: true })
+  this.anims.create({ key: 'attack', frames: attackFrames, frameRate: 7, repeat: 0 })
+  this.anims.create({ key: 'sheathe', frames: sheatheFrames, frameRate: 7, repeat: 0 })
 
-  const adventurer = this.add.sprite(100, 370, 'adventurer', 'adventurer-slide-00.png')
+  adventurer = this.add.sprite(100, 370, 'adventurer', 'adventurer-slide-00.png')
   adventurer.setScale(4, 4)
   adventurer.anims.play('idle')
-
-  this.tweens.add({
-    targets: adventurer,
-    x: { from: 100, to: 800 },
-    delay: 5000,
-    onStart: () => {
-      adventurer.anims.play('run')
-    },
-    onRepeat: () => {
-      adventurer.setScale(4, 4)
-    },
-    onYoyo: () => {
-      adventurer.setScale(-4, 4)
-    },
-    ease: 'Linear',
-    duration: 3000,
-    repeat: -1,
-    yoyo: true
-  })
+  adventurer.on('animationcomplete', function (anim, frame) {
+    if(anim.key == 'jump') {
+      this.anims.play('idle')
+    }
+    if(anim.key == 'attack') {
+      this.anims.play('sheathe')
+    }
+    if(anim.key == 'sheathe') {
+      this.anims.play('idle')
+    }
+  }, adventurer)
 }
